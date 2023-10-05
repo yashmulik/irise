0a064852dbe5a2a42fd2b3782021af07e83e8513
Yash Mulik <96407502+yashmulik@users.noreply.github.com>
2023-10-05 18:24:21 +0530

    updates

---
+++ day-1-js/day-1-js.js
@@ -0,0 +1,49 @@
+// datatypes
+
+// String
+// syntax-> "",'',``
+
+console.log(typeof("kuch bhi bolna hay"))
+console.log(typeof("lets define with singlr coats"))
+console.log(typeof("now lets use backtics"))
+console.log(typeof("please switch on the fan at 10 am & now"))
+console.log(typeof("77769"))
+console.log(typeof("*&^%$"))
+console.log(typeof("true"))
+
+// Number
+
+// Syntax-> 77769;
+
+console.log(typeof(77769))
+
+// Boolean
+
+// syntax-> true/false;
+
+console.log(typeof(true))
+
+// varibles
+
+// var 
+// let
+// const
+
+var yash=7776928691
+console.log(yash)
+console.log(typeof(yash))
+
+var irise="hello guys"
+console.log(irise)
+console.log(typeof(irise))
+
+var isstudentcompletedassingment=true;  //true/false
+console.log(isstudentcompletedassingment)
+console.log(typeof(isstudentcompletedassingment))
+
+// operations
+
+var number1=23;
+var number2=98;
+var addition= number1 + number2;
+console.log(addition,"-addition")
