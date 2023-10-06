558b8efb5313e83b392318979e102738b9c1a188
Yash Mulik <96407502+yashmulik@users.noreply.github.com>
2023-10-06 16:46:08 +0530

    updates

---
+++ day-3-js/day-3-js.js
@@ -0,0 +1,34 @@
+var number = 87;
+
+// console.log(87 / 2)// que
+// console.log(87 % 2)// remin
+// console.log(86 / 2)// que
+
+// console.log(83 % 2) 
+// console.log(84 % 2)
+// console.log(85 % 2)
+// console.log(86 % 2)
+
+// console.log(56 % 2 == 0)
+// console.log(57 % 2 == 0)
+
+// var number = 87;
+// if(number % 2 == 0){
+//     console.log(`the ${number}is even`)
+// }
+// else{
+//     console.log(`the ${number} is odd`)
+// }
+
+// datatypes
+
+// var mynumber
+// console.log("typeof(mynumber)") //undefined datatypes
+
+
+
+Q-given an a number find out is number even or odd. if number is even then check number1 and if number is odd check number3 ?
+
+var number1 =23; 
+var number2 = 78;
+var number3 = 67;
