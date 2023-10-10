95009c4d258ab648a43887142ce7875a2571db9e
Yash Mulik <96407502+yashmulik@users.noreply.github.com>
2023-10-10 21:44:37 +0530

    updates

--- day-3-js/day-3-js.js
+++ day-3-js/day-3-js.js
@@ -27,8 +27,26 @@ var number = 87;
 
 
 
-Q-given an a number find out is number even or odd. if number is even then check number1 and if number is odd check number3 ?
+// Q-given an a number find out is number even or odd. if number is even then check number1 and if number is odd check number3 ?
 
 var number1 =23; 
 var number2 = 78;
 var number3 = 67;
+
+if(number % 2 == 0){
+    if (number2 % 2 == 0){
+        console.log(`$(number1) and $(number2) are even number.`)
+    }
+    else{
+        console.log(`$(number1) is even but and $(number) is odd.`)
+    }
+} 
+else{
+    if (number3 % 2 == 0){
+        console.log(`$(number3) is even and $(number1) is odd.`)
+    }else{
+        console.log(`$(number3) and $(number1) both are odd`)
+    }
+
+}
+
---
+++ day-4-js/day-4-js.js
@@ -0,0 +1,30 @@
+// // loops
+// // for
+
+// Q-print numbers from 0-1000.
+
+// var number = 1;
+// number++
+// number++
+// number++
+// console.log(number)
+
+// var number = 1;
+// number++
+// number++
+// number--
+// console.log(number)
+
+
+// for(starting;ending;sequence ){
+    
+// }
+
+// for (i=0; i<10; i++){
+//     console.log(i)
+// }
+
+
+for (i=10; i>=0; i++){    //reverse loop
+    console.log(i)
+}
