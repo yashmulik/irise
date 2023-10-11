696ef0d5595ab8d2fe4af115198539bd1ae6e612
Yash Mulik <96407502+yashmulik@users.noreply.github.com>
2023-10-11 16:03:17 +0530

    updates

---
+++ day-5-js/day-5-js.js
@@ -0,0 +1,27 @@
+// var student1 = "yash"
+// var student2 = "pratik"
+// var student3 = "virat"
+
+// Array
+
+// const studentlist = ["yash", "pratik", "virat"]
+// console.log(studentlist)
+
+// const list = ["yash",25, true, studentlist]
+// console.log(list)
+
+// var list =['a', 'b', 'c', 'd'];
+// console.log(list)
+// console.log(list[0])
+// console.log(list[1])
+// console.log(list[2])
+// console.log(list[3])
+// console.log(list.length)
+
+var students = ['yash', 'pratik', 'virat'];
+
+for(var i = 0; i<students.length; i++){
+    // console.log(i);
+    console.log(students[i]);
+    
+}
